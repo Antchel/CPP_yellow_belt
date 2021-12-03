5ecff09a760b7f457092a1b5d08ba78a847e2c54
Anton <golan349@gmail.com>
2021-12-03 23:49:39 +0500

    1 week blocks

--- 1_Week/03_basic_types_and_type_casting/01_blocks_weight/01_blocks_weight.cpp
+++ 1_Week/03_basic_types_and_type_casting/01_blocks_weight/01_blocks_weight.cpp
@@ -1,11 +1,17 @@
-
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int blocksCount = 0;
-    cin >> blocksCount;
-    
+	int qty = 0, dsty = 0;
+	cin >> qty >> dsty;
+	int mass=0;
+	while (qty--)
+	{
+		int w, d, h;
+		cin >> w >> h >> d;
+		mass += dsty * w * h * d;
+	}
+	cout << mass << endl;
 }
