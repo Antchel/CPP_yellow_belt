22aacb0a8f703d60ef37744c3eb125ba4576ea34
Anton <golan349@gmail.com>
2021-12-07 23:31:03 +0500

    1 week Task_tracker 95 % complete

--- 1_Week/03_basic_types_and_type_casting/04_Task_tracker/04_Task_tracker.cpp
+++ 1_Week/03_basic_types_and_type_casting/04_Task_tracker/04_Task_tracker.cpp
@@ -19,17 +19,65 @@ public:
 	}
 
 	void AddNewTask(const string& person) {
-		if (storage.find(person) == storage.end()) {
-			storage[person] = { {TaskStatus::DONE, 0},
-								{TaskStatus::IN_PROGRESS, 0},
-								{TaskStatus::NEW, 0},
-								{TaskStatus::TESTING, 0} };
-		}
 		storage[person][TaskStatus::NEW]++;
 	}
 
 	tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string& person, int task_count) {
-		return { {}, {} };
+		if (storage.find(person) == storage.end())
+			return { {},{} };
+		TaskStatus task = TaskStatus::NEW;
+		TasksInfo updatedTasks, untouchedTasks;
+		untouchedTasks = storage[person];
+		untouchedTasks.erase(TaskStatus::DONE);
+		bool is_tasksEnd = false;
+		while (task_count--) {
+			switch (task)
+			{
+			case TaskStatus::NEW:
+				if (storage[person][TaskStatus::NEW] != 0) {
+					storage[person][TaskStatus::NEW]--;
+					storage[person][TaskStatus::IN_PROGRESS]++;
+					untouchedTasks[TaskStatus::NEW]--;
+					updatedTasks[TaskStatus::IN_PROGRESS]++;
+				}
+				else {
+					task = TaskStatus::IN_PROGRESS;
+					untouchedTasks.erase(TaskStatus::NEW);
+				}
+				break;
+			case TaskStatus::IN_PROGRESS:
+				if (storage[person][TaskStatus::IN_PROGRESS] != 0) {
+					storage[person][TaskStatus::IN_PROGRESS]--;
+					storage[person][TaskStatus::TESTING]++;
+					untouchedTasks[TaskStatus::IN_PROGRESS]--;
+					updatedTasks[TaskStatus::TESTING]++;
+				}
+				else {
+					task = TaskStatus::TESTING;
+					untouchedTasks.erase(TaskStatus::IN_PROGRESS);
+				}
+				break;
+			case TaskStatus::TESTING:
+				if (storage[person][TaskStatus::TESTING] != 0) {
+					storage[person][TaskStatus::TESTING]--;
+					storage[person][TaskStatus::DONE]++;
+					untouchedTasks[TaskStatus::TESTING]--;
+					updatedTasks[TaskStatus::DONE]++;
+				}
+				else {
+					task = TaskStatus::DONE;
+					untouchedTasks.erase(TaskStatus::TESTING);
+					is_tasksEnd = true;
+				}
+				break;
+			case TaskStatus::DONE:
+			default:
+				break;
+			}
+			if (is_tasksEnd == true) break;
+		}
+
+		return { updatedTasks, untouchedTasks};
 	}
 
 private:
@@ -37,11 +85,11 @@ private:
 
 };
 
-void PrintTasksInfo(const TasksInfo& info) {
-	cout << info.at(TaskStatus::NEW) << " new tasks" << " , " <<
-		info.at(TaskStatus::IN_PROGRESS) << " tasks in progress" << " , " <<
-		info.at(TaskStatus::TESTING) << " tasks are been testing" << " , " <<
-		info.at(TaskStatus::DONE) << " tasks are been done" << endl;
+void PrintTasksInfo(TasksInfo info) {
+	cout << info[TaskStatus::NEW] << " new tasks" << " , " <<
+		info[TaskStatus::IN_PROGRESS] << " tasks in progress" << " , " <<
+		info[TaskStatus::TESTING] << " tasks are been testing" << " , " <<
+		info[TaskStatus::DONE] << " tasks are been done" << endl;
 }
 
 int main()
@@ -49,10 +97,31 @@ int main()
 	try {
 		TeamTasks tasks;
 		TasksInfo info;
+		tasks.AddNewTask("Ilia");
 		tasks.AddNewTask("Ivan");
 		tasks.AddNewTask("Ivan");
 		tasks.AddNewTask("Ivan");
+		cout << "Ilia's tasks: ";
+		PrintTasksInfo(tasks.GetPersonalTaskInfo("Ilia"));
+		cout << "Ivan's tasks: ";
 		PrintTasksInfo(tasks.GetPersonalTaskInfo("Ivan"));
+
+		TasksInfo updated_tasks, untouched_tasks;
+
+		tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan", 2);
+		cout << "Updated Ivan's tasks: ";
+		PrintTasksInfo(updated_tasks);
+		cout << "Untouched Ivan's tasks: ";
+		PrintTasksInfo(untouched_tasks);
+
+		tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan", 2);
+		cout << "Updated Ivan's tasks: ";
+		PrintTasksInfo(updated_tasks);
+		cout << "Untouched Ivan's tasks: ";
+		PrintTasksInfo(untouched_tasks);
+
+
+
 	} catch (exception& e) {
 		cout << e.what() << endl;
 	}
