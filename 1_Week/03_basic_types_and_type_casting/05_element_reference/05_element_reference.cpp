be2ed4f7801955fae06be12c4c44e8995979b1bb
Anton <golan349@gmail.com>
2021-12-09 22:55:34 +0500

    1 week elem ref

---
+++ 1_Week/03_basic_types_and_type_casting/05_element_reference/05_element_reference.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <exception>
+#include <utility>
+#include <map>
+
+using namespace std;
+
+template <typename First, typename Second>
+Second& GetRefStrict( map<First, Second>& m, First key) {
+	if (m.count(key) == 0) {
+		throw runtime_error("There is no element by this key");
+	}
+	Second& val = m.at(key);
+	return val;
+}
+
+int main()
+{
+	try {
+		map<int, string> m = { {0, "zero string"} };
+		string& item = GetRefStrict(m, 0);
+		item = "new_value";
+		cout << m[0] << endl;
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
+	
+}
